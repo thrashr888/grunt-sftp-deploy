f77ce8d8995fde0f309c732d7051f96dc711961d
Martin Heidegger <martin.heidegger@gmail.com>
2013-04-16 14:33:34 +0900

    Changed mkdir setting from permissions to mode
    ssh2 module changed property from "permissions" to "mode"
    
    https://github.com/mscdex/ssh2/commit/2081bff14a7e58195758cb9990e8cb8db1a3c1a6

--- tasks/sftp-deploy.js
+++ tasks/sftp-deploy.js
@@ -128,7 +128,7 @@ module.exports = function(grunt) {
     files = toTransfer[inPath];
     remotePath = remoteRoot + (inPath == '/' ? inPath : path.sep + inPath);
 
-    sftpConn.mkdir(remotePath, {permissions: 775}, function(err) {
+    sftpConn.mkdir(remotePath, {mode: 775}, function(err) {
       console.log('mkdir ' + remotePath, err ? 'error or dir exists' : 'ok');
 
       // console.log(async);
