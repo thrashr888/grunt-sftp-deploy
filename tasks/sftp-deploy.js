0a4e70fefe68eb77aca5dd92eedfaf11ed6a1820
Louis Teboul <louis.teboul@izi-pass.com>
2014-07-25 10:14:37 +0200

    getAuthByKey: shorthand statements & better error catching

--- tasks/sftp-deploy.js
+++ tasks/sftp-deploy.js
@@ -143,25 +143,19 @@ module.exports = function(grunt) {
     });
   }
 
-  function getAuthByKey (inKey) {
-    if (process.env[inKey]) {
-      return JSON.parse(process.env[inKey]);
+  function getAuthByKey(inKey) {
+    if (inKey !== null) {
 
-    } else if (fs.existsSync(inKey)) {
-      var tmpStr = grunt.file.read(inKey);
-      var retVal = null;
-      if (inKey !== null && tmpStr.length) retVal = JSON.parse(tmpStr);
-      return retVal;
+      if (process.env[inKey]) {
+        return JSON.parse(process.env[inKey]);
 
-    } else {
-      var tmpStr;
-      var retVal = null;
+      } else if (fs.existsSync(inKey)) {
+        return JSON.parse(grunt.file.read(inKey)) || null;
 
-      if (fs.existsSync('.ftppass')) {
-        tmpStr = grunt.file.read('.ftppass');
-        if (inKey !== null && tmpStr.length) retVal = JSON.parse(tmpStr)[inKey];
+      } else if (fs.existsSync('.ftppass')) {
+        return JSON.parse(grunt.file.read('.ftppass'))[inKey] || null;
       }
-      return retVal;
+
     }
   }
 
